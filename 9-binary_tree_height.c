f576715a69118b804a98f4a985356a892aa75b6c
deenakamal <deenah.kamal@gmail.com>
2024-02-29 14:32:12 -0800

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (!tree->left || !tree->right)
+	if (!tree->left)
+		return (0);
+	if (!tree->right)
 		return (0);
 
 	left_height = 1 + binary_tree_height(tree->left);
