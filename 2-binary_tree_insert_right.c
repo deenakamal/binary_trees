d2c96b6c8a5e213e8c1c01fe6b058b48d7b23398
deenakamal <deenah.kamal@gmail.com>
2024-02-29 13:47:55 -0800

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_right - inserts a node as the right-child of another node.
+ * binary_tree_insert_right - inserts a node as the right-child.
  *
  * @parent: pointer to the node to insert the right-child in.
  *
